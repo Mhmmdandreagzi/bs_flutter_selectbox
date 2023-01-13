52754c04aab0853027ee059e8e4d167ef07366ec
heru <thedreamer.19@gmail.com>
2023-01-13 13:42:16 +0700

    fixing bug glass blur

--- lib/src/customize/bs_dialogbox_style.dart
+++ lib/src/customize/bs_dialogbox_style.dart
@@ -1,21 +1,25 @@
 import 'package:flutter/material.dart';
 
 class BsDialogBoxStyle {
-
-  const BsDialogBoxStyle({
-    this.itemColor,
-    this.itemTextColor,
-    this.borderRadius,
-    this.backgroundColor,
-    this.border,
-    this.boxShadow
-  });
+  const BsDialogBoxStyle(
+      {this.itemColor,
+      this.itemTextColor,
+      this.borderRadius,
+      this.backgroundColor,
+      this.border,
+      this.glasBlur,
+      this.glasColor,
+      this.boxShadow});
 
   final Color? itemColor;
 
   final Color? itemTextColor;
 
   final Color? backgroundColor;
+
+  final Color? glasColor;
+
+  final double? glasBlur;
 
   final BorderRadius? borderRadius;
 
